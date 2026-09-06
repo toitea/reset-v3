97a99698c54dbbf9a881ddf3f2ebe766983c54b3
toitea <toicarr01@gmail.com>
2026-09-06 14:36:33 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyCCR9U7bRROoowXH9BFskNVJe-dddW79bM",
+  authDomain: "reset-86dfa.firebaseapp.com",
+  projectId: "reset-86dfa",
+  storageBucket: "reset-86dfa.firebasestorage.app",
+  messagingSenderId: "348000226684",
+  appId: "1:348000226684:web:b8604dd0127015aa9aa8f9"
 };
 
 export const vapidKey = "PASTE_YOUR_WEB_PUSH_VAPID_PUBLIC_KEY";
