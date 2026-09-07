b1c19cde7ea656aaa31f17a4afed27b8a41909bd
toitea <toicarr01@gmail.com>
2026-09-07 00:35:31 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='reset-v10-softluxe1';
+const C='reset-v11-big-redesign-20260907-1';
 const A=['./','./index.html','./data.js','./firebase-config.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))])));
@@ -6,5 +6,9 @@ self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   const u=new URL(e.request.url);
   if(u.origin!==location.origin)return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+  if(u.pathname.endsWith('/')||u.pathname.endsWith('/index.html')){
+      e.respondWith(fetch(new Request(e.request,{cache:'no-store'})).then(r=>r).catch(()=>caches.match('./index.html')));
+      return;
+    }
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
 });
